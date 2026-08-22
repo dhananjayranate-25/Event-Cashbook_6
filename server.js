7b24aa771fa069c188b94ac84f26fca5c0078ee0
Dhananjay Ranate <dhananjayranate25@gmail.com>
2026-08-22 22:33:04 +0530

    Add custom 404 error page and routing in server.js

--- server.js
+++ server.js
@@ -1231,6 +1231,11 @@ app.delete('/api/users/:id/photo', async (req, res) => {
     }
 });
 
+// Handle 404 Pages
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, '404.html'));
+});
+
 app.listen(PORT, () => {
     console.log(`Ganpati Vargani Cashbook running at http://localhost:${PORT}`);
     console.log(`Database: MongoDB`);
